828edb21e94763888cbd3438881a057e5a4ef9e8
UtkarshDixit-py <utkarshdixit303@gmail.com>
2022-05-25 13:14:30 +0530

    Update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey:"AIzaSyASu--lgSOVa6dMYzFekN2HDh7dOkg7r20" ,
+  authDomain:"rc-app-bd223.firebaseapp.comm" ,
+  databaseURL:"https://rc-app-bd223.firebaseio.com" ,
+  projectId:"rc-app-bd223",
+  storageBucket:"rc-app-bd223" ,
+  messagingSenderId:"482659143331",
+  appId:"1:482659143331:web:8d4cf900c3908ec0e33916",
 };
 
 // Initialize Firebase
